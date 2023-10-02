9d1edf25e059cab9c761a8a656452f02c3a847b1
michael <michaelngige@gmail.com>
2023-10-02 08:50:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+    // To pass the tests, don't forget to return your fetch!
+    
+   fetch("https://anapioficeandfire.com/api/books")
+      .then((resp) => resp.json())
+      .then((json) => renderBooks(json));
+
+    
   
 }
 
@@ -15,3 +23,8 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
